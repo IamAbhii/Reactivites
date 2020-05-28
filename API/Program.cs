1ef692a1d380f19593cd25cd1f0e312988eaf99a
abhi <abrahmbhatt03@gmai.com>
2020-05-28 16:43:10 -0500

    Added CRUD operation on client side.

--- API/Program.cs
+++ API/Program.cs
@@ -6,39 +6,31 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Persistence;
 
-namespace API
-{
-  public class Program
-  {
-    public static void Main(string[] args)
-    {
+namespace API {
+  public class Program {
+    public static void Main (string[] args) {
 
-      var host = CreateHostBuilder(args).Build();
-      using (var scope = host.Services.CreateScope())
-      {
+      var host = CreateHostBuilder (args).Build ();
+      using (var scope = host.Services.CreateScope ()) {
         var services = scope.ServiceProvider;
-        try
-        {
-          var context = services.GetRequiredService<DataContext>();
-          context.Database.Migrate();
-          Seed.SeedData(context);
+        try {
+          var context = services.GetRequiredService<DataContext> ();
+          context.Database.Migrate ();
+          Seed.SeedData (context);
 
-        }
-        catch (Exception ex)
-        {
-          var logger = services.GetRequiredService<ILogger<Program>>();
-          logger.LogError(ex, "An error occured during migration");
+        } catch (Exception ex) {
+          var logger = services.GetRequiredService<ILogger<Program>> ();
+          logger.LogError (ex, "An error occured during migration");
         }
       }
-      host.Run();
+      host.Run ();
 
     }
 
-    public static IHostBuilder CreateHostBuilder(string[] args) =>
-        Host.CreateDefaultBuilder(args)
-            .ConfigureWebHostDefaults(webBuilder =>
-            {
-              webBuilder.UseStartup<Startup>();
-            });
+    public static IHostBuilder CreateHostBuilder (string[] args) =>
+      Host.CreateDefaultBuilder (args)
+      .ConfigureWebHostDefaults (webBuilder => {
+        webBuilder.UseStartup<Startup> ();
+      });
   }
 }
